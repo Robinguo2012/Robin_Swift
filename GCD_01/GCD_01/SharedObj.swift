222a63ba3b6c6428f524f61fb5c5ce49d60b3d71
郭华斌 <guohb@walkingtec.cn>
2016-07-22 14:40:43 +0800

    commit

---
+++ GCD_01/GCD_01/SharedObj.swift
@@ -0,0 +1,14 @@
+//
+//  SharedObj.swift
+//  GCD_01
+//
+//  Created by sks on 16/7/22.
+//  Copyright © 2016年 sks. All rights reserved.
+//
+
+import Foundation
+
+class SharedObj: NSObject {
+
+    
+}
--- GCD_01/GCD_01/ViewController.swift
+++ GCD_01/GCD_01/ViewController.swift
@@ -100,14 +100,171 @@ class ViewController: UIViewController {
 //        operation.start()
         
         
+        /*
+         dispatch_barrier_async
+         *我们先将一个9初始化到NSUserDefaults的Integer_Key中，然后在中间执行dispatch_barrier_async函数，由于这个队列是一个Concurrent Dispatch Queue，能同时并发多少线程是由系统决定的，如果添加dispatch_barrier_async的时候，其他的block（包括上面4个block）还没有开始执行，那么会先执行dispatch_barrier_async里的任务，其他block全部处于等待状态。如果添加dispatch_barrier_async的时候，已经有block在执行了，那么dispatch_barrier_async会等这些block执行完后再执行。
+         */
+        NSUserDefaults.standardUserDefaults().setInteger(9, forKey: "Integer_key")
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
+            () -> Void in
+            self.readFile();
+        })
+        
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
+            () -> Void in
+            self.readFile();
+        })
+        
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
+            () -> Void in
+            self.readFile();
+        })
+        
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
+            () -> Void in
+            self.readFile();
+        })
+        
+        dispatch_barrier_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
+            () -> Void in
+            self.writeFile()
+        })
+        
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
+            () -> Void in
+            self.readFile();
+        })
+        
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
+            () -> Void in
+            self.readFile();
+        })
+        
+        /*
+         dispatch_apply
+         dispatch_apply会将一个指定的block执行指定的次数。
+         如果要对某个数组中的所有元素执行同样的block的时候，这个函数就显得很有用了，用法很简单，指定执行的次数以及Dispatch Queue，在block回调中会带一个索引，然后就可以根据这个索引来判断当前是对哪个元素进行操作
+         */
+        
+        let globalQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
+
+        
+        dispatch_apply(10, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
+            (index) -> Void in
+            print("\(index)")
+        })
+        
+        print("completed")
+        
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
+            () -> Void in
+            dispatch_apply(10, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
+               (index) -> Void in
+                print("\(index)")
+            })
+        })
+        
+        print("completed again")
+        /*
+         * dispatch_suspend / dispatch_resume
+         *
+         *
+         */
+        dispatch_suspend(globalQueue) //暂停一个队列
+        dispatch_resume(globalQueue)  //恢复一个队列
+        
+        // dispatch_semaphore 信号量
+        let semaphore = dispatch_semaphore_create(1)
+        let time = dispatch_time(DISPATCH_TIME_NOW,  (Int64)(3 * NSEC_PER_SEC))
+        
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
+            () -> Void in
+            
+            dispatch_semaphore_wait(semaphore, time)
+            
+            print("需要线程同步的操作1开始")
+            sleep(2)
+            print("需要线程同步的操作1结束")
+        
+            dispatch_semaphore_signal(semaphore)
+        })
+        
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
+            () -> Void in
+            
+            sleep(1);
+            dispatch_semaphore_wait(semaphore, time)
+            print("需要线程同步的操作2")
+            dispatch_semaphore_signal(semaphore)
+        })
+        
+        let semaphore1 = dispatch_semaphore_create(1)
+        
+        for _ in 1...9 {
+            
+            dispatch_async(globalQueue, {
+            () -> Void in
+                dispatch_semaphore_wait(semaphore1, DISPATCH_TIME_FOREVER)
+                
+                let time1 = dispatch_time(DISPATCH_TIME_NOW, (Int64)(NSEC_PER_SEC * 2))
+                
+                dispatch_after(time1, globalQueue, {
+                () -> Void in
+                   print("2秒后执行")
+                    dispatch_semaphore_signal(semaphore1)
+                })
+            })
+        }
+        
+        let singletonObj = SingletonObject.sharedInstance
+        
+        singletonObj.name = "郭华斌"
+        
+        print("name is \(singletonObj.name)")
+        
+        let singleton1 = SingletonObject.sharedInstance
+        
+        singleton1.name = "从叶"
+        
+        print("name pre is \(singletonObj.name)")
+        print("name is \(singleton1.name)")
+
         
     }
-
+    
+    
+    func writeFile() {
+        NSUserDefaults.standardUserDefaults().setInteger(7, forKey: "Integer_key")
+    }
+    
+    func readFile()  {
+        NSUserDefaults.standardUserDefaults().integerForKey("Integer_key")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
 
+}
+
+class SingletonObject {
+    var name:String = ""
+    var age = 10
+    
+    class var sharedInstance: SingletonObject{
+        
+        struct Struct {
+            static var once_token: dispatch_once_t = 0
+            static var instance: SingletonObject? = nil
+        }
+        
+        dispatch_once(&Struct.once_token){
+            Struct.instance = SingletonObject()
+        }
+        
+        return Struct.instance!
+    }
 }
 
